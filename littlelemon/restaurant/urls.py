588afa984a914e2d4b10297c6dce9bbbe3f979ea
Molzhigitov2004 <molzhigitov04@gmail.com>
2026-01-17 19:49:32 +0500

    minor tweaks

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -4,8 +4,8 @@ from . import views
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [ 
-    path('menu/', views.MenuItemView.as_view(), name = 'menu-list'),
-    path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('menu-items/', views.MenuItemView.as_view(), name = 'menu-list'),
+    path('menu-items/<int:pk>', views.SingleMenuItemView.as_view()),
     path('tables', views.BookingView.as_view()),
     path('tables/<int:pk>', views.SingleBookingView.as_view()),
     path('api-token-auth/', obtain_auth_token),
--- littlelemon/tests/test_models.py
+++ littlelemon/tests/test_models.py
@@ -5,4 +5,4 @@ class MenuItemTest(TestCase):
     def test_get_item(self):
         item = MenuTable.objects.create(title="VainllaIce", price=80, inventory=100)
         self.assertEqual(str(item), "VainllaIce : 80")
-#this is not a default location lol
+#this is not a default location lol i places my tests in app lvl
